5e413ab29bb79d5ad85dbf0ff44005bfd48569f0
JoeySchmoe <narakuian19@gmail.com>
2022-03-03 13:15:19 -0700

    Clean up unused JS | Add score display

--- javascript.js
+++ javascript.js
@@ -10,6 +10,9 @@ buttons[0].addEventListener('click', () => playGame('rock'));
 buttons[1].addEventListener('click', () => playGame('paper'));
 buttons[2].addEventListener('click', () => playGame('scissors'));
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let rand = Math.random();
     let choice = "";
@@ -76,6 +79,7 @@ function playGame(player) {
             case 'paper':
                 winnerText = l;
                 resultText = 'loses to';
+                computerScore++;
                 break;
 
             case 'scissors':
@@ -137,67 +141,3 @@ function capitalize(string) {
     let capitalizedString = firstLetter + lowerString.slice(1);
     return capitalizedString;
 }
-
-function getPlayerChoice() {
-    //return window.prompt("Enter your choice:");
-}
-
-function playRound(player, computer) {
-    let pChoice = player.toUpperCase();
-    let cChoice = computer.toUpperCase();
-    let pWin = "You Win!";
-    let cWin = "You Lose!";
-    let tie = "Tie!";
-
-    if (pChoice === "ROCK") {
-        switch (cChoice) {
-            case "ROCK":
-                return `${tie} Rock ties with Rock.`;
-            
-            case "PAPER":
-                return `${cWin} Rock loses to Paper.`
-
-            case "SCISSORS":
-                return `${pWin} Rock beats Scissors.`;
-        }
-    } else if (pChoice === "PAPER") {
-        switch (cChoice) {
-            case "ROCK":
-                return `${pWin} Paper beats Rock.`;
-
-            case "PAPER":
-                return `${tie} Paper ties with Paper.`;
-
-            case "SCISSORS":
-                return `${cWin} Paper loses to Scissors.`;
-        }
-
-    } else if (pChoice === "SCISSORS") {
-        switch (cChoice) {
-            case "ROCK":
-                return `${cWin} Scissors loses to Rock.`;
-
-            case "PAPER":
-                return `${pWin} Scissors beats Paper.`;
-
-            case "SCISSORS":
-                return `${tie} Scissors ties with Scissors.`;
-        }
-
-    } else {
-        console.log("ERROR: pChoice doesn't match any expected values");    
-    }
-
-}
-
-
-
-// function playGame(rounds) {
-//     for (var i = 0; i < rounds; i++) {
-//         let playerChoice = getPlayerChoice();
-//         let computerChoice = getComputerChoice();
-//         console.log(playRound(playerChoice, computerChoice));
-//     }
-// }
-
-//playGame(window.prompt("How many rounds to play:"));
